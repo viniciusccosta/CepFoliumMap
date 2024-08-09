9b602d62999d319235c089935cfe62a7dcb82025
viniciusccosta <viniciusccosta95@gmail.com>
2024-08-09 15:36:40 -0300

    Changing logging level from others modules.

--- cepfoliummap/config.py
+++ cepfoliummap/config.py
@@ -7,14 +7,17 @@ from rich.logging import RichHandler
 
 
 def config_logging():
-    # Configurando logging:
+    # Handlers:
     file_handler = logging.FileHandler(
+        encoding="utf-8",
         filename=f"logs/{datetime.now():%Y-%m-%d-%H-%M-%S}.log",
         mode="w",
-        encoding="utf-8",
     )
-    stream_handler = RichHandler(rich_tracebacks=True)
+    stream_handler = RichHandler(
+        rich_tracebacks=True,
+    )
 
+    # Logging:
     logging.basicConfig(
         level=config("LOGGING_LEVEL", logging.INFO),
         encoding="utf-8",
@@ -25,14 +28,26 @@ def config_logging():
         ],
     )
 
+    # Outros loggers:
+    logging.getLogger("httpcore.http11").setLevel(logging.WARNING)
+    logging.getLogger("httpcore.connection").setLevel(logging.WARNING)
+    logging.getLogger("httpx").setLevel(logging.INFO)
+
+    logging.debug("Logging configurado")
+
 
 def create_directories():
+    logging.debug("Criando diretórios")
+
     Path("mapas/").mkdir(parents=True, exist_ok=True)
     Path("brasilapi/").mkdir(parents=True, exist_ok=True)
     Path("logs/").mkdir(parents=True, exist_ok=True)
     Path("geodecode/").mkdir(parents=True, exist_ok=True)
     Path("merges/").mkdir(parents=True, exist_ok=True)
 
+    logging.debug("Diretórios criados")
+
 
 def initial_config():
+    config_logging()
     create_directories()
